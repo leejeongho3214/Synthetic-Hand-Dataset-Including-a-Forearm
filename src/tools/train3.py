5f5b27bb230d51fd741aaa38259e50e26171d6c3
leejeongho3214 <wowo933@naver.com>
2023-07-15 10:35:35 +0900

    update 230715

--- src/tools/train3.py
+++ src/tools/train3.py
@@ -2,16 +2,29 @@ import gc
 import os
 import sys
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  # Arrange GPU devices starting from 0
-os.environ["CUDA_VISIBLE_DEVICES"]= "3" 
-os.environ["TF_ENABLE_ONEDNN_OPTS"]="0"
+os.environ["MKL_THREADING_LAYER"] = "GNU"
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+
+import torch
 from torch.utils import data
 from src.utils.argparser import parse_args, load_model, train, valid
 from dataset import *
 from src.utils.bar import colored
 
-def worker(world_rank, world_size, nodes_size, args, trainset_loader, valset_loader, epo):
+def worker(world_rank, world_size, nodes_size, args, trainset_loader, valset_loader):
+
+    _model, best_loss, epo, count, writer = load_model(args)
+    pck_l = 0; batch_time = AverageMeter()
+    print(colored("Train_len: {}, Val_len: {}".format(len(train_dataset), len(val_dataset)), "blue"))
+    
+    args.logger.debug("Train_len: {}, Val_len: {}".format(len(train_dataset), len(val_dataset)))
+    if world_rank >= 0:
+        assert torch.cuda.is_available() and torch.cuda.device_count() > world_rank
+        torch.cuda.empty_cache()
+        args.device = torch.device("cuda", world_rank)
+        
     if world_size > 1:
         torch.distributed.init_process_group(
             backend="nccl", init_method="tcp://localhost:23456" if nodes_size == 1 else "env://",
@@ -39,26 +52,22 @@ def worker(world_rank, world_size, nodes_size, args, trainset_loader, valset_loa
         torch.cuda.empty_cache()
 
 def main(args):
-
+        
     train_dataset, val_dataset = build_dataset(args)
 
     trainset_loader = data.DataLoader(dataset=train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
     valset_loader = data.DataLoader(dataset=val_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False)
-
-    _model, best_loss, epo, count, writer = load_model(args)
-    pck_l = 0; batch_time = AverageMeter()
-    print(colored("Train_len: {}, Val_len: {}".format(len(train_dataset), len(val_dataset)), "blue"))
-    args.logger.debug("Train_len: {}, Val_len: {}".format(len(train_dataset), len(val_dataset)))
+    
     
     device_ids = list(map(int, args.device_ids.split(",")))
     nprocs = len(device_ids)
     
     if nprocs > 1:
         torch.multiprocessing.spawn(
-            worker, args=(nprocs, 1, args,  trainset_loader, valset_loader, epo), nprocs=nprocs,
+            worker, args=(nprocs, 1,  args, trainset_loader, valset_loader, train_dataset, val_dataset), nprocs=nprocs,
             join=True)
     elif nprocs == 1:
-        worker(device_ids[0], nprocs, 1, args, trainset_loader, valset_loader, epo)
+        worker(device_ids[0], nprocs, 1, args, trainset_loader, valset_loader, train_dataset, val_dataset )
     else:
         assert False
     
